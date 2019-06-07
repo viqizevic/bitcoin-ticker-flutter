6284688c29d564071d76e72d96f9213db6dd447c
Vicky Tanzil <vicky.tanzil@gmail.com>
2019-06-08 00:02:52 +0200

    Add picker

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart' as coin;
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +9,42 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String currency = 'EUR';
+  String crypto = 'BTC';
+  double value = 10000.57;
+
+  DropdownButton<String> getAndroidDropdown() {
+    return DropdownButton(
+      items: coin.currenciesList.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      onChanged: (String newValue) {
+        setState(() {
+          currency = newValue;
+        });
+      },
+      value: currency,
+    );
+  }
+
+  CupertinoPicker getIOSPicker() {
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        setState(() {
+          currency = coin.currenciesList.elementAt(selectedIndex);
+        });
+      },
+      children: coin.currenciesList.map<Text>((String value) {
+        return Text(value);
+      }).toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +66,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 $crypto = ${value.toString()} $currency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,7 +81,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? getIOSPicker() : getAndroidDropdown(),
           ),
         ],
       ),
